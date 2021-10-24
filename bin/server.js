d0faa70d3741c34eceaed7dd52d7d27ef2539a97
João Walter Amadeu <joaowa2002@live.com>
2021-10-24 18:59:38 -0300

    Implementando o uso de servidor https

--- bin/server.js
+++ bin/server.js
@@ -1,15 +1,27 @@
 'use strict'
 
-//console.log('Teste');
+const cert = process.env.certfile;
+const certkey = process.env.certkey;
+
 const app = require('../src/app')
-const http = require('http');
+if (!cert) {
+    const http = require('http');
+} else {
+    const https = require('https');
+}
 const { normalize } = require('path');
 
 const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port); 
-
-const server=http.createServer(app);
-
+app.set('port', port);
+var server;
+if (!cert) {
+    server = http.createServer(app);
+} else {
+    server = https.createServer({
+        key: fs.readFileSync(certkey),
+        cert: fs.readFileSync(cert),
+    }, app);
+}
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
@@ -19,18 +31,18 @@ console.log('Rodando na porta ' + port);
 function normalizePort(val) {
     const port = parseInt(val, 10);
 
-    if(isNaN(port)){
+    if (isNaN(port)) {
         return val;
     }
-    if(port >= 0){
+    if (port >= 0) {
         return port;
     }
     return false;
 }
 
-function onError(e){
+function onError(e) {
     'Erro:' + e;
 }
-function onListening(e){
+function onListening(e) {
     'Pronto';
 }
