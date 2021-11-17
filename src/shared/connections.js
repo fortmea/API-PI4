9cc015f1eab5b36efadc384e6a36938b60796404
João Walter Amadeu <joaowa2002@live.com>
2021-11-17 17:53:56 -0300

    Update connections.js

--- src/shared/connections.js
+++ src/shared/connections.js
@@ -15,7 +15,6 @@ exports.databaseCredentials = {
 };
 async function initialize() {
     // create db if it doesn't already exist
-    const { host, port, user, password, database } = config.database;
     const connection = await mysql.createConnection({ host, port, user, password });
     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
