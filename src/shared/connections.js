e74b9ff1c5d079a53d8a0ca248810c97c9489855
João Walter Amadeu <joaowa2002@live.com>
2021-11-17 18:04:48 -0300

    remove database creation

--- src/shared/connections.js
+++ src/shared/connections.js
@@ -13,21 +13,6 @@ exports.databaseCredentials = {
     password: password,
     dialect: 'mysql'
 };
-async function initialize() {
-    // create db if it doesn't already exist
-    const connection = await mysql.createConnection({ host, port, user, password, database });
-    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
-
-    // connect to db
-    const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
-
-    // init models and add them to the exported db object
-    db.User = require('../users/user.model')(sequelize);
-
-    // sync all models with database
-    await sequelize.sync();
-}
-initialize();
 const {Sequelize, Model, DataTypes} = require('sequelize');
 exports.sequelize = new Sequelize(
     exports.databaseCredentials.database,
@@ -38,4 +23,4 @@ exports.sequelize = new Sequelize(
         dialect: exports.databaseCredentials.dialect
     });
 
-exports.Usuario = require('../models/cliente').init(exports.sequelize, Sequelize);
+exports.Usuario = require('../models/lista').init(exports.sequelize, Sequelize);
