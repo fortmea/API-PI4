3fb2916d3218f39ab85f0abe7e569650acb47156
João Walter Amadeu <joaowa2002@live.com>
2021-11-17 22:52:25 -0300

    Update server.js

--- bin/server.js
+++ bin/server.js
@@ -22,6 +22,11 @@ if (!cert) {
         cert: fs.readFileSync('/home/admin/key/fullchain.pem'),
     }, app);
 /*}*/
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  });
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
