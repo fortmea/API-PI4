bd06ea74e6b294ed98e933a501dd78e43ab3f1ab
João Walter Amadeu <joaowa2002@live.com>
2021-11-17 23:06:59 -0300

    Update server.js

--- bin/server.js
+++ bin/server.js
@@ -4,7 +4,6 @@ const cert = process.env.certfile;
 const certkey = process.env.certkey;
 const cors = require('cors');
 const app = require('../src/app');
-app.use(cors());
 const http = require('http');
 const https = require('https');
 const { normalize } = require('path');
@@ -16,6 +15,7 @@ app.set('port', port);
         cert: fs.readFileSync('/home/admin/key/fullchain.pem'),
     }, app);
 server.listen(port);
+server.use(cors());
 server.on('error', onError);
 server.on('listening', onListening);
 console.log('Rodando na porta ' + port);
