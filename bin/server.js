3b8051aca0dc76bbcc36fb274b25b003fcec47d9
João Walter Amadeu <joaowa2002@live.com>
2021-11-17 23:19:32 -0300

    Update server.js

--- bin/server.js
+++ bin/server.js
@@ -15,7 +15,11 @@ app.set('port', port);
         cert: fs.readFileSync('/home/admin/key/fullchain.pem'),
     }, app);
 server.listen(port);
-app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 server.on('error', onError);
 server.on('listening', onListening);
 console.log('Rodando na porta ' + port);
